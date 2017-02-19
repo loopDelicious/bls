577c2f4a40b9307375efad52c36a9592857ab644
Joyce Lin <joycelin79@gmail.com>
2017-02-19 14:36:48 -0800

    format bls seriesid

--- src/javascripts/city.js
+++ src/javascripts/city.js
@@ -8,8 +8,35 @@ class City extends Component {
     state = {
         startyear: new Date().getFullYear() - 2,
         endyear: new Date().getFullYear(),
-        seriesid: [ 'LAUCN040010000000005', 'LAUCN040010000000006' ]
+        seriesid: [ 'APU000070111', 'NCU0099573300000' ]
     };
+
+    // SeriesID: NCU5306633300003
+    // NC - prefix
+    // U - unadjusted for seasonality
+    // 53 - state code
+    // 0663 - area code
+    // 33000 - occupation code (1 of 480 census occupations)
+    // 03 - level code
+    // SeriesID for no level Computer Programmers in San Francisco-Oakland-San Jose, CA :  NCU0607251722900
+
+    // Geography ****************************
+    // All US: 00	9957
+    // Seattle: 53	0745 (Seattle-Tacoma-Bremerton, WA)
+    // San Francisco: 06	0725 (San Francisco-Oakland-San Jose, CA)
+    // Los Angeles: 06	0445 (Los Angeles-Riverside-Orange County, CA)
+    // Chicago: 17	0155 (Chicago-Gary-Kenosha, IL-IN-WI)
+    //      17, 18, 55
+    // Denver: 08	0205 (Denver-Boulder-Greeley, CO)
+    // Austin: 48	0064 (Austin-San Marcos, TX)
+    // New York: 09	0553 (New York-Northern New Jersey-Long Island, NY-NJ-CT-PA)
+    //      34, 36, 42, 09
+    // Boston: 23	0105 (Boston-Worcester-Lawrence, MA-NH-ME-CT)
+    //      25, 23, 33, 09
+
+    // Occupation ****************************
+    // Computer Programmers: 17	229
+
 
     handleDisplay = () => {
 
